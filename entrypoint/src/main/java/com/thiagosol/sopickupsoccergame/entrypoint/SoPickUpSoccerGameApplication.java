ceb32cccf7e3aeea87831201aa0c317e856ffd3e
Thiago Sol <thiagosol@ymail.com>
2022-01-15 17:34:21 -0300

    Start and modularization

---
+++ entrypoint/src/main/java/com/thiagosol/sopickupsoccergame/entrypoint/SoPickUpSoccerGameApplication.java
@@ -0,0 +1,13 @@
+package com.thiagosol.sopickupsoccergame.entrypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoPickUpSoccerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoPickUpSoccerGameApplication.class, args);
+	}
+
+}
